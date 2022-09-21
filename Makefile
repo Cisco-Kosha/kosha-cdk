afb048698bb97c0ee05a0f303090f7dda4d6c3f1
aditigu2 <aditigu2@cisco.com>
2022-09-20 21:56:22 -0400

    A lot of template code -- including custom auth

---
+++ Makefile
@@ -0,0 +1,4 @@
+build: 
+	go build -o bin/generate main.go
+
+all: build
--- main.go
+++ main.go
@@ -1,24 +1,55 @@
 package main
 
 import (
-	"path/filepath"
-	"os"
-	"text/template"
-	"strings"
-	"fmt"
-	"encoding/json"
-	"io/ioutil"
-	"regexp"
 	"bufio"
-
-	// conf "github.com/kosha/kosha-cdk/config"
-	// "github.com/kosha/kosha-cdk/utils"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"text/template"
+	// "golang.org/x/text/cases"
+	// "golang.org/x/text/language"
 )
 
-var nonGoFiles = []string{"/go.mod"}
+// var nonGoFiles = []string{"/go.mod"}
 
 type Config struct {
-	ConnectorName string `json:"connector_name,omitempty"`
+	ConnectorName string      `json:"connector_name,omitempty"`
+	Auth          Auth        `json:"auth,omitempty"`
+	Prometheus    Prometheus  `json:"prometheus,omitempty"`
+	Endpoints     []Endpoints `json:"endpoints,omitempty"`
+}
+
+type Auth struct {
+	Type       string   `json:"type,omitempty"`
+	Domain     string   `json:"domain,omitempty"`
+	Spec       []string `json:"spec,omitempty"`
+	MeEndpoint string   `json:"me_endpoint,omitempty"`
+}
+
+type MeEndpoint struct {
+	Path   string `json:"path,omitempty"`
+	Method string `json:"method,omitempty"`
+}
+
+type Prometheus struct {
+	Enabled string `json:"enabled,omitempty"`
+}
+
+type Endpoints struct {
+	Name                string      `json:"name,omitempty"`
+	Description         string      `json:"description,omitempty"`
+	ApiPath             string      `json:"api_path,omitempty"`
+	Method              string      `json:"method,omitempty"`
+	KoshaPath           string      `json:"kosha_path,omitempty"`
+	QueryParameters     []string    `json:"query_parameters,omitempty"`
+	PathParameters      []string    `json:"path_parameters,omitempty"`
+	EnableAllParameters bool        `json:"enableAllParameters,omitempty"`
+	Tag                 string      `json:"tag,omitempty"`
+	Body                interface{} `json:"body,omitempty"`
 }
 
 func copyFile(source_path string, dest_path string) {
@@ -32,13 +63,47 @@ func copyFile(source_path string, dest_path string) {
 	}
 }
 
-func contains(elems []string, v string) bool {
-    for _, s := range elems {
-        if v == s {
-            return true
-        }
-    }
-    return false
+func removeUnderscores(s string) string {
+	var fragments []string
+	for _, frag := range strings.Split(s, "_") {
+		fragments = append(fragments, strings.Title(frag))
+	}
+	return strings.Join(fragments, "")
+}
+
+func insertPathParameters(path string) {
+	// r := regexp.MustCompile(`\{(.*?)\}`)
+	// matches := r.FindAllString(path, -1)
+	// for _, match := range matches {
+	// 	match := strings.Trim(match, "{")
+	// 	trimmed := strings.Trim(match, "}")
+	// 	re := regexp.MustCompile(`\{` + trimmed + `\}`)
+	// 	path = re.ReplaceAllString(path, vars[trimmed])
+	// }
+	// fmt.Println(path)
+
+	r := regexp.MustCompile(`\{(.*?)\}`)
+	matches := r.FindAllString(path, -1)
+
+	for _, match := range matches {
+		m := regexp.MustCompile(match)
+		i := m.FindStringIndex(path)
+		fmt.Println(i)
+	}
+
+	//Note: return a list with each substring
+	//before match
+	//match replaced with
+
+	//note to self: just do id
+	//split path into "pre-id" and "post-id"
+	//then return a list of strings
+	//make it so it's "pre_id" + id + "post_id"
+
+	//maybe seperate function for pre_id
+	//seperate function for post id to make it easy to do
+
+	//{{pre_id function}} + id + {{post_id_function}}
 }
 
 func populateTemplateFiles(source_dir string, dest_dir string, config *Config) {
@@ -46,34 +111,31 @@ func populateTemplateFiles(source_dir string, dest_dir string, config *Config) {
 	dest_dir = "../" + dest_dir
 	os.MkdirAll(dest_dir, 0755)
 
-	err := filepath.Walk("./" + source_dir, func(path string, info os.FileInfo, err error) error {
+	//Add custom template functions
+	funcMap := template.FuncMap{
+		"ToUpper":           strings.ToUpper,
+		"Title":             strings.Title,
+		"RemoveUnderscores": removeUnderscores,
+	}
+
+	err := filepath.Walk("./"+source_dir, func(path string, info os.FileInfo, err error) error {
 		fileInfo, _ := os.Stat(path)
 		re := regexp.MustCompile(source_dir + "\\s*(.*?)$")
 		fileBase := re.FindStringSubmatch(path)[1]
-		fmt.Println(fileBase)
 		//If it is a directory, copy it
 		if fileInfo.IsDir() {
-			fmt.Println("I AM IN IS DIR" + path)
 			os.MkdirAll(filepath.Join(dest_dir, fileBase), 0755)
 		} else {
-			fmt.Println("I AM NOT" + path)
 			//If it is a template file, read and populate tags
 			if strings.Contains(path, ".tmpl") {
 				re = regexp.MustCompile(source_dir + "\\s*(.*?)\\s*.tmpl")
 				fileBase = re.FindStringSubmatch(path)[1]
-				fmt.Println(fileBase)
 				//Read in file and execute template
 				buf, _ := ioutil.ReadFile(path)
-				tmpl, _ := template.New("connectorFiles").Parse(string(buf))
-				
+				tmpl, _ := template.New("connectorFiles").Funcs(funcMap).Parse(string(buf))
+
 				//Generate target file
 				targetFile := filepath.Join(dest_dir, fileBase)
-				// if ! utils.contains(conf.nonGoFiles, fileBase) {
-				// 	targetFile = targetFile + ".go"
-				// }
-				if ! (contains(nonGoFiles, fileBase)) {
-					targetFile = targetFile + ".go"
-				}
 				f, err := os.OpenFile(targetFile, os.O_WRONLY|os.O_CREATE, 0755)
 				if err != nil {
 					return err
@@ -104,74 +166,5 @@ func main() {
 	json.Unmarshal(configFile, &config)
 	destination_dir := config.ConnectorName + "-connector"
 	populateTemplateFiles("repo-structure", destination_dir, &config)
+	// insertPathParameters("/projects/api/{version}/projects/{id}.json")
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// func ParseTemplates() []string {
-// 	var filepaths []string
-//     err := filepath.Walk("./repo-structure", func(path string, info os.FileInfo, err error) error {
-//         if strings.Contains(path, ".tmpl") {
-//             filepaths = append(filepaths, path)
-//             if err != nil {
-//             	fmt.Println(err)
-//             }
-//         }
-//     })
-
-//     if err != nil {
-//         panic(err)
-//     }
-
-//     return filepaths
-// }
-
-// func ParseDirectory(baseDir string) {
-// 	t, err := template.New("templ").ParseGlob(path + "/templates/*.html")
-// }
-
-
-// func main() {
-// 	connectorData := Input{"teamwork"}
-// 	filePaths := ParseTemplates()
-// 	for _, file := range filePaths {
-
-// 	}
-
-//     t := template.New("main.tmpl").ParseFiles(filePaths)
-// 	err := t.Execute(os.Stdout, connectorData)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// }
-
-// func main() {
-// 	connectorData := Input{"teamwork"}
-// 	if err := os.Mkdir("connector", os.ModePerm); err != nil {
-//         log.Fatal(err)
-//     }
-// 	err := filepath.Walk("./repo-structure", func(path string, info os.FileInfo, err error) error {
-//         file, err := os.Open(path)
-// 		if err != nil {
-// 			panic(err)
-// 		}
-// 		fileInfo, err := file.Stat()
-// 		if err != nil {
-// 			panic(err)
-// 		}
-// 		if fileInfo.IsDir() {
-
-// 		}
-//     })
-// }
